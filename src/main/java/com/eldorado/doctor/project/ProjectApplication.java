527f5eaf3f2147cff5eefa0042f93e18db71de38
Raidy Machado <raidy_silva@hotmail.com>
2020-04-22 18:39:26 -0400

    docker

--- src/main/java/com/eldorado/doctor/project/ProjectApplication.java
+++ src/main/java/com/eldorado/doctor/project/ProjectApplication.java
@@ -2,13 +2,20 @@ package com.eldorado.doctor.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 //@SpringBootApplication(exclude = {SecurityAutoConfiguration.class }) //Not Security
 @RestController
-public class ProjectApplication {
+public class ProjectApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectApplication.class, args);
